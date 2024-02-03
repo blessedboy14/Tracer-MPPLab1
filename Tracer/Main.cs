f6bbe7c30cfbefc4af1198e2702ca89edc68f65a
blessedboy14 <92112019+blessedboy14@users.noreply.github.com>
2024-02-03 18:49:51 +0300

    kinda add all functionality

--- Tracer/Main.cs
+++ Tracer/Main.cs
@@ -4,26 +4,23 @@ using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
+using Tracing;
 
 class TracerLab
 {
-    public static ITracer tracer = new CustomTracer();
-    public static Foo _foo;
+    public static CustomTracer tracer = new CustomTracer();
     static public void Main(string[] args)
     {
-        _foo = new Foo(tracer);
-        _foo.MyMethod();
-        tracer.GetTraceResult();
+        exampleMethod();
+        TraceResult res = tracer.GetTraceResult();
         int i = 0;
     }
 
     static public void exampleMethod()
     {
         tracer.StartTrace();
-        for (long i = 0; i < 100000000; i++)
-        {
-            var t = (i / 2 * i / 3) % 50 * 100;
-        }
+        Thread.Sleep(2000);
         tracer.StopTrace();
     }
 }
--- Tracer/Tracing/CustomTracer.cs
+++ Tracer/Tracing/CustomTracer.cs
@@ -17,19 +17,34 @@ namespace Tracing
         public Stopwatch watch;
         private ConcurrentDictionary<int, List<Stopwatch>> timers = new ConcurrentDictionary<int, List<Stopwatch>>();
         private TraceResult info = new TraceResult();
-        private ConcurrentDictionary<int, int> methods = new ConcurrentDictionary<int, int>();
+        private ConcurrentDictionary<int, int> methodsInThread = new ConcurrentDictionary<int, int>();
 
-        private readonly object balanceLock = new object();
-
-        private TraceResult traceResult;
-
-        public CustomTracer()
+        private void CreateMethodStack(int threadID, string methodName, string className)
         {
-            watch = new Stopwatch();
+            List<MethodTrace> methodList = new List<MethodTrace>();
+            methodList = info.threads[threadID].methodsTrace;
+            for (int i = 1; i < methodsInThread[threadID];i++)
+            {
+                methodList = methodList[methodList.Count - 1].stack;
+            }
+            MethodTrace methodInfo = new MethodTrace();
+            methodInfo.methodName = methodName;
+            methodInfo.className = className;
+            methodList.Add(methodInfo);
         }
 
         public TraceResult GetTraceResult()
         {
+            foreach(KeyValuePair<int, ThreadTrace> thread in info.threads)
+            {
+                long time = 0;
+                foreach(MethodTrace method in thread.Value.methodsTrace) 
+                {
+                    time += method.executeInMillis;
+                }
+                thread.Value.executeTime = time;
+            }
+            return info;
         }
 
         public void StartTrace()
@@ -44,13 +59,28 @@ namespace Tracing
             if (info.threads.TryAdd(threadID, curTrace))
             {
                 info.threads[threadID].threadId = threadID;
-                methods.TryAdd(threadID, 0);
+                methodsInThread.TryAdd(threadID, 0);
                 timers.TryAdd(threadID, new List<Stopwatch>());
             }
+            methodsInThread[threadID]++;
+            timers[threadID].Add(new Stopwatch());
+            CreateMethodStack(threadID, methodName, className);
+            timers[threadID][timers[threadID].Count - 1].Start();
         }
 
         public void StopTrace()
         {
+            int threadID = Thread.CurrentThread.ManagedThreadId;
+            timers[threadID][timers[threadID].Count - 1].Stop();
+            List<MethodTrace> methodsList = new List<MethodTrace>();
+            methodsList = info.threads[threadID].methodsTrace;
+            for (int i = 1; i < methodsInThread[threadID]; i++)
+            {
+                methodsList = methodsList[methodsList.Count - 1].stack;
+            }
+            methodsList[methodsList.Count - 1].executeInMillis = timers[threadID][timers[threadID].Count - 1].ElapsedMilliseconds;
+            timers[threadID].Remove(timers[threadID][timers[threadID].Count - 1]);
+            methodsInThread[threadID]--;
         }
     }
 }
